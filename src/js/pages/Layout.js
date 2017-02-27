55efd2ca400d993580d0f78f591ac657caf62c21
Rico <richardthomaswhitfield@gmail.com>
2017-02-27 17:24:26 -0600

    part1 complete

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -5,10 +5,18 @@ import Footer from "../components/layout/Footer";
 import Nav from "../components/layout/Nav";
 
 export default class Layout extends React.Component {
+  navigate() {
+    console.log("this.props", this.props);
+    this.props.history.replaceState(null, "/");
+  }
   render(){
     return (
         <div>
           <h1>KillerNews.net</h1>
+          {this.props.children}
+          <Link to="archives"><button class="btn btn-primary">archives</button></Link>
+          <Link to="settings"><button class="btn btn-primary">settings</button></Link>
+          <button onClick={this.navigate.bind(this)}>featured</button>
           </div>
       );
   }
