eb5cebd4a46f33e13f4c2121dbf16412ab41990a
Rico <richardthomaswhitfield@gmail.com>
2017-02-27 21:02:56 -0600

    completed react spa tut

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react_spa_tut
+[react spa tutorial] (https://www.youtube.com/watch?v=1iAG6h9ff5s)
--- src/js/client.js
+++ src/js/client.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Layout from "./pages/Layout";
+import Archives from "./pages/Archives";
 import Featured from "./pages/Featured";
+import Layout from "./pages/Layout";
 import Settings from "./pages/Settings";
-import Archived from "./pages/Archived";
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
-		<Router history={hashHistory}>
-		<Route path="/" component={Layout}>
-		</Route>
-		<IndexRoute component={Featured}></IndexRoute>
-		<Route path="archives/(:article)" component={Archives}></Route>
-		<Route path="settings" component={Settings}></Route>
-		</Router>,
-	app)
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+  </Router>,
+app);
